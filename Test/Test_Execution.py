093636994a85eea240802f2a556de5455c2a2ac1
Vidhya-Arjun <vidhyasarjun@gmail.com>
2026-03-30 12:51:08 +0530

    Formy form submission-changes for wait

--- Test/Test_Execution.py
+++ Test/Test_Execution.py
@@ -1,9 +1,6 @@
 import time
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import wait, expected_conditions
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
